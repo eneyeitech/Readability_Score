36ab9aac6941c5f52d8a7d80604a1553a308fd7b
Abdulmumin Eneye Abdulkarim <abdulmumineabdulkarim@gmail.com>
2022-03-15 19:18:25 +0100

    Stage 1/4: Simplest estimation

--- Readability Score/task/src/readability/Main.java
+++ Readability Score/task/src/readability/Main.java
@@ -2,6 +2,11 @@ package readability;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello world!");
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        String sentence = scanner.nextLine();
+
+        String verdict = sentence.length() > 100 ? "HARD" : "EASY";
+
+        System.out.println(verdict);
     }
 }
